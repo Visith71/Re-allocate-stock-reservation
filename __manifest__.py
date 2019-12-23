afa2edb186c689cfa7cbedf087f2aa2f7a6d7d31
tslai <tl@quartile.co>
2019-12-23 13:49:20 +0800

    [UPD] Improve coding style

--- __manifest__.py
+++ __manifest__.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-
 {
-    'name': 'Manufacturing_extension',
+    'name': 'Re-allocate Stock Reservation of Manufacturing Orders',
     'version': '12.0.1.0.0',
     'Author': 'SAM VISITH',
-    'website':'',
+    'website': '',
     'category': 'Manufacturing',
-    'sequence': 16,
     'summary': 'Allow to re-allocate stock reservation to more urgent MOs',
     'depends': ['mrp'],
-    'description': """This module provides the following features:
-                    1. Not allow negative stock.
-                    2. Allow to re-allocate stock reservation to more urgent MOs. 
-               """,
+    'description': """
+This module provides the following features:
+1. Not allow negative stock.
+2. Allow to re-allocate stock reservation to more urgent MOs. 
+""",
     'data': [
-        "views/mrp_production_view.xml",
+        'views/mrp_production_views.xml',
+        'views/stock_move_line_views.xml',
     ],
     'installable': True,
     'application': True,
