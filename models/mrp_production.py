204a4d4f99f47ef269b75bbf8338f398fe3329e6
Visith71 <samvisith16@kit.edu.kh>
2019-12-25 10:12:56 +0700

    ommit button_mark_don() method

--- models/mrp_production.py
+++ models/mrp_production.py
@@ -7,15 +7,6 @@ class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     @api.multi
-    def button_mark_done(self):
-        self.ensure_one()
-        for order in self:
-            for m in order.move_raw_ids:
-                if int(m.qty_available - m.product_uom_qty) < 0:
-                    raise UserError(_('You cannot produce a MO with a negative stock move.'))
-        return super(MrpProduction, self).button_mark_done()
-
-    @api.multi
     def post_inventory(self):
         self.ensure_one()
         for order in self:
