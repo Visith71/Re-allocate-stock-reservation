f87651bec6edf1d1cf2cb318153b8ed95b3405d1
Visith71 <samvisith16@kit.edu.kh>
2019-12-25 09:21:31 +0700

    change the sequence of super() in the orride function

--- models/mrp_production.py
+++ models/mrp_production.py
@@ -9,33 +9,19 @@ class MrpProduction(models.Model):
     @api.multi
     def button_mark_done(self):
         self.ensure_one()
-        super(MrpProduction, self).button_mark_done()
         for order in self:
             for m in order.move_raw_ids:
                 if int(m.qty_available - m.product_uom_qty) < 0:
                     raise UserError(_('You cannot produce a MO with a negative stock move.'))
+        super(MrpProduction, self).button_mark_done()
         return self.write({'state': 'done', 'date_finished': fields.Datetime.now()})
-    
-    @api.multi
-    def open_produce_product(self):
-        self.ensure_one()
-        super(MrpProduction, self).open_produce_product()
-        for order in self:
-            all_reserved_availability = all([int(m.reserved_availability) <= 0 for m in order.move_raw_ids])
-            any_reserved_availability = any([int(m.reserved_availability) <= 0 for m in order.move_raw_ids])
-            if all_reserved_availability:
-                raise UserError(_('You cannot produce a MO without checking availability.'))
-            elif any_reserved_availability:
-                raise Warning(_("Not enough invetory! Some components are not available.\nPlease increase your stock."))
-        action = self.env.ref('mrp.act_mrp_product_produce').read()[0]
-        return action
 
     @api.multi
     def post_inventory(self):
         self.ensure_one()
-        super(MrpProduction, self).post_inventory()
         for order in self:
             for m in order.move_raw_ids:
                 if int(m.qty_available - m.product_uom_qty) < 0:
                     raise UserError(_('You cannot produce a MO with a negative stock move.'))
+        super(MrpProduction, self).post_inventory()
         return True
